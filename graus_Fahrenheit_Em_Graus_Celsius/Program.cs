b366b8b95df47be9cfa87058587db0157037eadd
FireDragonSrc <firedragonsrc@gmail.com>
2023-10-17 11:27:29 -0300

    commit inicial

---
+++ graus_Fahrenheit_Em_Graus_Celsius/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graus_Fahrenheit_Em_Graus_Celsius
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Graus Fahrenheit:");
+            float fahrenheit = int.Parse(Console.ReadLine());
+            float Celsius = Conversor(fahrenheit);
+            Console.WriteLine(Celsius);
+            Console.ReadLine();
+        }
+        static float Conversor(float fahrenheit)
+        {
+            float grausCelsius = (fahrenheit - 32f) * 5f / 9f;
+            return grausCelsius;
+        }
+    }
+}
---
+++ graus_Fahrenheit_Em_Graus_Celsius/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("graus_Fahrenheit_Em_Graus_Celsius")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("graus_Fahrenheit_Em_Graus_Celsius")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("fbad2806-e0bf-4231-b271-02cb5822f375")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
